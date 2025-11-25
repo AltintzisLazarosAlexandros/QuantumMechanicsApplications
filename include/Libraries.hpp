4ff569dab51653f1425620118d3fb508239f629c
Altintzis Lazaros Alexandros <csd5358@csd.uoc.gr>
2025-11-25 14:57:44 +0200

    Add CMake configuration and update Libraries.hpp for Boost, GSL, and QuEST integration

--- include/Libraries.hpp
+++ include/Libraries.hpp
@@ -1,11 +1,31 @@
 #ifndef LIBRARIES_HPP
 #define LIBRARIES_HPP
 
+// Standard C++ libraries
 #include <iostream>
 #include <vector>
 #include <string>
 #include <map>
+#include <cmath>
+#include <complex>
+#include <algorithm>
+
+// Linear Algebra - Eigen
 #include <Eigen/Dense>
 
+// Numerical Computations - Boost
+#include <boost/math/special_functions.hpp>
+#include <boost/numeric/odeint.hpp>
+#include <boost/array.hpp>
+
+// Scientific Computing - GSL
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_eigen.h>
+#include <gsl/gsl_sf_legendre.h>
+#include <gsl/gsl_integration.h>
+
+// Quantum Circuit Simulator - QuEST (optional, uncomment if installed)
+// #include <QuEST.h>
 
 #endif // LIBRARIES_HPP
